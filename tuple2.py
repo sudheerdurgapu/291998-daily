b4d2ea3ddc75e8be63e9ac0673b5c0b3affdd8ba
sudheerdurgapu <42888007+sudheerdurgapu@users.noreply.github.com>
2021-04-23 13:44:20 +0530

    Delete tuple2.py

--- tuple2.py
+++
@@ -1,4 +0,0 @@
-#Write a Python program to convert a list of tuples into a dictionary
-tuples = [('Key 1', 1), ('Key 2', 2), ('Key 3', 3), ('Key 4', 4), ('Key 5', 5)]
-result = dict(tuples)
-print(result)
