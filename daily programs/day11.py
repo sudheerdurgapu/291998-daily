43625bd2dd90abf35de90be0cafa036b5ce371ae
sudheerdurgapu <42888007+sudheerdurgapu@users.noreply.github.com>
2021-04-26 15:50:12 +0530

    Add files via upload

---
+++ daily programs/day11.py
@@ -0,0 +1,16 @@
+class Person(object):
+    def __init__(self, name, idnumber):
+        self.name = name
+        self.idnumber = idnumber
+    def display(self):
+        print(self.name)
+        print(self.post)
+        print(self.idnumber)
+class Employee(Person):
+    def __init__(self, name, idnumber, salary, post):
+        self.salary = salary
+        self.post = post
+        Person.__init__(self, name, idnumber)
+a = Employee('Stepin', 291998,10000, "genisis")
+a.display()
+
